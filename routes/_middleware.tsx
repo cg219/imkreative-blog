26051706249edb8d4b0706d868e4e7cd2f08490c
Clemente <dev@imkreative.com>
2022-08-16 20:24:52 -0400

    Setup Redirect Middleware

--- routes/_middleware.tsx
+++ routes/_middleware.tsx
@@ -2,6 +2,12 @@ import { Handlers, MiddlewareHandlerContext } from "$fresh/server.ts";
 
 export function handler (req: Request, ctx: MiddlewareHandlerContext) {
     const url = new URL(req.url);
-    console.log(url);
-    return ctx.next();
+
+    if (url.hostname == Deno.env.get('IMK_REDIRECT_FROM')) {
+        url.hostname = Deno.env.get('IMK_REDIRECT_TO')!;
+
+        return Response.redirect(url, 302);
+    } else {
+        return ctx.next();
+    }
 }
