2bf263003695ce378b7d9f8f6759271fe1da5039
Clemente <dev@imkreative.com>
2022-09-11 04:51:40 -0400

    Update project settings

--- components/HomeLayout.tsx
+++ components/HomeLayout.tsx
@@ -6,7 +6,7 @@ export function HomeLayout(props: HomeLayoutProps) {
   return (
     <html lang="en">
       <head>
-        <meta charset="utf-8" />
+        <meta charSet="utf-8" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=5, user-scalable=0"
@@ -28,7 +28,7 @@ export function HomeLayout(props: HomeLayoutProps) {
         <link rel="canonical" href={props.url || Deno.env.get('IMK_REDIRECT_TO')!} />
         <link rel="stylesheet" type="text/css" href="css/normalize.css" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link rel="stylesheet" type="text/css" href="css/index.css" />
         <link
           href="https://fonts.googleapis.com/css2?family=Exo:wght@800&family=Mukta&display=swap"
