7169d76d3ab3c05ee668ecc348f3e0f083125fa1
Mente Gee <dev@imkreative.com>
2025-05-18 17:01:43 -0400

    use DenoKv for data

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,14 +1,16 @@
 export async function load() {
-    const API_URL = Deno.env.get("IMK_API_URL");
-    const API_KEY = Deno.env.get("IMK_API_KEY");
-    const API_VERSION = "v4";
     const url = Deno.env.get("IMK_REDIRECT_TO");
-    const postFetch = await fetch(`${API_URL}/ghost/api/content/posts/?key=${API_KEY}&include=tags,authors`);
-    const postData = await postFetch.json();
-    const settingsFetch = await fetch( `${API_URL}/ghost/api/content/settings/?key=${API_KEY}`);
-    const settingsData = await settingsFetch.json();
-    const posts = postData.posts
-    const settings = settingsData.settings
+    const kv = await Deno.openKv();
+    // const kv = await Deno.openKv("https://api.deno.com/databases/b410a3cf-acb8-4017-9008-8b12fddf1d4c/connect");
+    const postsRes = await kv.list({ prefix: ["posts"]})
+    const rawPosts = await Array.fromAsync(postsRes)
+    const posts = rawPosts.map((d) => d.value).sort((a, b) => {
+        if (a.published_at > b.published_at) return -1
+        if (a.published_at < b.published_at) return 1
+        return 0
+    })
+    const settingsRes = await kv.get(["settings"])
+    const settings = settingsRes.value
 
     return {
         posts,
--- src/routes/read/[slug]/+page.server.ts
+++ src/routes/read/[slug]/+page.server.ts
@@ -1,20 +1,15 @@
 export async function load({ params }) {
-    const API_URL = Deno.env.get("IMK_API_URL");
-    const API_KEY = Deno.env.get("IMK_API_KEY");
-    const API_VERSION = "v4";
     const url = Deno.env.get("IMK_REDIRECT_TO");
-    const postFetch = await fetch(`${API_URL}/ghost/api/content/posts/slug/${params.slug}?key=${API_KEY}&include=tags,authors&limit=1`);
-    const postData = await postFetch.json();
-    const settingsFetch = await fetch( `${API_URL}/ghost/api/content/settings/?key=${API_KEY}`);
-    const settingsData = await settingsFetch.json();
-    const posts = postData.posts
-    const settings = settingsData.settings
-    const navigation = settings.navigation
+    const kv = await Deno.openKv();
+    // const kv = await Deno.openKv("https://api.deno.com/databases/b410a3cf-acb8-4017-9008-8b12fddf1d4c/connect");
+    const postsRes = await kv.get(["posts", params.slug])
+    const posts = [postsRes.value] 
+    const settingsRes = await kv.get(["settings"])
+    const settings = settingsRes.value
 
     return {
         posts,
         settings,
-        navigation,
-        url 
+        url
     }
 }
