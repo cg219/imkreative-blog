bb5e30b3d12cab67698589d45adcbb5f4963bd64
Mente Gee <dev@imkreative.com>
2025-08-24 11:35:00 -0400

    use denokv for settings as well

--- src/routes/portfolio/+page.server.ts
+++ src/routes/portfolio/+page.server.ts
@@ -10,10 +10,13 @@ const client = createClient({
 export async function load() {
     const API_URL = Deno.env.get("IMK_API_URL");
     const API_KEY = Deno.env.get("IMK_API_KEY");
-    const settingsFetch = await fetch( `${API_URL}/ghost/api/content/settings/?key=${API_KEY}`);
-    const settingsData = await settingsFetch.json();
-    const settings = settingsData.settings;
-
+    const kv = await Deno.openKv();
+    // const kv = await Deno.openKv("https://api.deno.com/databases/b410a3cf-acb8-4017-9008-8b12fddf1d4c/connect");
+    // const settingsFetch = await fetch( `${API_URL}/ghost/api/content/settings/?key=${API_KEY}`);
+    // const settingsData = await settingsFetch.json();
+    // const settings = settingsData.settings;
+    const settingsRes = await kv.get(["settings"])
+    const settings = settingsRes.value
     const projects = await client.fetch(`*[_type == 'site'] {
         date,
         active,
